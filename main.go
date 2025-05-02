35ac0eb339929ab1f221c1abe014c8fbe5e8c219
WasathTheekshana <wasath.vt@gmail.com>
2025-05-02 23:04:04 +0530

    feat: inputs with args function

--- main.go
+++ main.go
@@ -4,17 +4,40 @@ import (
 	"fmt"
 	"os"
 	"bufio"
+	"strings"
+	"os/exec"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
+		fmt.Print(">")
+
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println("Error reading input: ", err)
 		}
+
+		if err = execInput(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
 	}
 }
 
-func execInput(input string) {}
+func execInput(input string) error {
+	// Remove the new line character
+	input = strings.TrimSuffix(input, "\n")
+
+	// Split the input to get the command and arguments
+	args := strings.Split(input, " ")
+
+	// Prepare the command to execute
+	cmd := exec.Command(args[0], args[1:]...)
+
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	// Execute the command
+	return cmd.Run()
+}

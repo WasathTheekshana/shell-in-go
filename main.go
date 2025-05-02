5ecddc3f6acf87c4f59814b306ecff5a9f033a96
WasathTheekshana <wasath.vt@gmail.com>
2025-05-02 23:22:09 +0530

    feat: change directory command set

--- main.go
+++ main.go
@@ -32,6 +32,17 @@ func execInput(input string) error {
 	// Split the input to get the command and arguments
 	args := strings.Split(input, " ")
 
+	switch args[0] {
+	case "exit":
+		os.Exit(0)		
+
+	case "cd":
+		if len(args) < 2 {
+			return fmt.Errorf("cd: Missing Arguments")
+		}
+		return os.Chdir(args[1])
+	}
+
 	// Prepare the command to execute
 	cmd := exec.Command(args[0], args[1:]...)
 
